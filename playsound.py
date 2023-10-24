c3fa220a7f9c5b6ca5567aaed22010fb616bd5e4
Sebastian-Alexis <s3b3999@gmail.com>
2023-10-24 15:59:08 -0700

    ADDED jumpscare script

---
+++ playsound.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+def play_loud_sound(file_path):
+    # Load the audio file
+    sound = AudioSegment.from_file(file_path, format="mp3")
+
+    # Boost the volume (you can adjust the value based on how loud you want it to be)
+    loud_sound = sound + 20  # Increase volume by 20dB
+
+    # Play the loud sound
+    play(loud_sound)
+
+
+if __name__ == "__main__":
+    play_loud_sound("jumpscare.mp3")
