1edb397e09212bc4bffc08b0ffff15eafac06350
Sebastian-Alexis <s3b3999@gmail.com>
2023-10-25 17:22:26 -0700

    improved jumpscare with customizable decibal range

--- playsound.py
+++ playsound.py
@@ -1,13 +1,16 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
+# increase by decibals, be careful not to damage ears or speakers
+volume = 20
+
 
 def play_loud_sound(file_path):
     # Load the audio file
     sound = AudioSegment.from_file(file_path, format="mp3")
 
     # Boost the volume (you can adjust the value based on how loud you want it to be)
-    loud_sound = sound + 20  # Increase volume by 20dB
+    loud_sound = sound + volume  # Increase volume by 20dB
 
     # Play the loud sound
     play(loud_sound)
